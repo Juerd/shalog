13a5791aa64ff84947688fc362ed5f2783d17029
Juerd Waalboer <juerd@tnx.nl>
2017-06-08 18:57:57 +0200

    MMD

--- lib/Color.pm
+++ lib/Color.pm
@@ -1,26 +1,14 @@
 unit module Color;
 
-sub red ($text?) is export {
-    return "\e[1;31m" without $text;
-    return "\e[1;31m$text\e[0m";
-}
-sub green ($text?) is export {
-    return "\e[1;32m" without $text;
-    return "\e[1;32m$text\e[0m";
-}
-sub yellow ($text?) is export {
-    return "\e[1;33m" without $text;
-    return "\e[1;33m$text\e[0m";
-}
-sub white ($text?) is export {
-    return "\e[0;1m" without $text;
-    return "\e[0;1m$text\e[0m";
-}
-sub no-color is export {
-    return "\e[0m";
-}
-sub reset-color is export {
-    print no-color;
-}
+multi sub red()      is export { "\e[1;31m" }
+multi sub red($t)    is export { "\e[1;31m$t\e[0m" }
+multi sub green()    is export { "\e[1;32m" }
+multi sub green($t)  is export { "\e[1;32m$t\e[0m" }
+multi sub yellow()   is export { "\e[1;33m" }
+multi sub yellow($t) is export { "\e[1;33m$t\e[0m" }
+multi sub white()    is export { "\e[0;1m" }
+multi sub white($t)  is export { "\e[0;1m$t\e[0m" }
+sub no-color()       is export { "\e[0m" }
+sub reset-color()    is export { print no-color; }
 
 # vim: ft=perl6
